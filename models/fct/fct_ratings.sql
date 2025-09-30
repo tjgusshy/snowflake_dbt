b005d19b632aa359457d18a8e1078a7a743f1514
e <tjt163787@gmail.com>
2025-09-30 15:23:28 +0100

    new file

--- models/fct/fct_ratings.sql
+++ models/fct/fct_ratings.sql
@@ -8,14 +8,17 @@ WITH src_ratings AS (
     SELECT * FROM {{ ref('src_ratings') }}
 )
 
+
 SELECT
-    user_id,
-    movie_id,
-    rating
+    src_ratings.user_id,
+    src_ratings.movie_id,
+    src_ratings.rating
 FROM src_ratings
 WHERE
-    rating IS NOT NULL
+    src_ratings.rating IS NOT NULL
     {% if is_incremental() %}
-        AND src_ratings.rating_timestamp
-        > (SELECT COALESCE(MAX(rating_timestamp), '1970-01-01') FROM {{ this }})
+        AND src_ratings.rating_timestamp > (
+            SELECT COALESCE(MAX(src_ratings.rating_timestamp), '1970-01-01')
+            FROM {{ src_ratings }}
+        )
     {% endif %}
