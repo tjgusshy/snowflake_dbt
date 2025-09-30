1c57f54a6bff45325a511d3c1dd6a866b41519cf
e <tjt163787@gmail.com>
2025-09-30 16:00:58 +0100

    git cicd

--- models/fct/fct_ratings.sql
+++ models/fct/fct_ratings.sql
@@ -1,5 +1,6 @@
 WITH src_ratings AS (
-    SELECT * FROM {{ ref('src_ratings') }}
+    SELECT *
+    FROM {{ ref('src_ratings') }}
 )
 
 SELECT
@@ -9,9 +10,9 @@ SELECT
 FROM src_ratings
 WHERE
     src_ratings.rating IS NOT NULL
-{% if is_incremental() %}
-    AND src_ratings.rating_timestamp > (
-        SELECT COALESCE(MAX(rating_timestamp), '1970-01-01')
-        FROM {{ this }}
-    )
-{% endif %}
+    {% if is_incremental() %}
+        AND src_ratings.rating_timestamp > (
+            SELECT COALESCE(MAX({{ this }}.rating_timestamp), '1970-01-01')
+            FROM {{ this }}
+        )
+    {% endif %}
